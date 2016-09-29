bffd3a6feb1680ff54679545589a3db1116968d8
Amir El-Sibaie <amir.elsibaie@gmail.com>
2016-09-28 20:42:01 -0400

    Basic user auth working

--- sqlite_db.py
+++ sqlite_db.py
@@ -1,6 +1,9 @@
-from time import time
+import time
+import email
 
 import hashlib
+import base64
+import os
 
 import sqlite3
 connection = sqlite3.connect('users.sqlite3', check_same_thread = False)
@@ -8,58 +11,80 @@ cursor = connection.cursor()
 
 # First, let's create the DB if it doesn't exist
 cursor.execute('''CREATE TABLE if not exists users (
-    username    TEXT    PRIMARY KEY,
-    email       TEXT,
-    salt        BLOB,
-    key         BLOB,
-    time        DATETIME,
-    admin       BOOLEAN,
-    validated   BOOLEAN)''')
+    username        TEXT        PRIMARY KEY,
+    email           TEXT,
+    salt            BLOB,
+    key             BLOB,
+    session_id      BLOB,
+    creationtime    DATETIME,
+    logintime       DATETIME,
+    admin           BOOLEAN,
+    validated       BOOLEAN)''')
 
-class User:
-    def __init__(self, username, email=None, salt=None, key=None, time=None, admin=False, validated=False, password=None):
-        self.username = username
-        self.email = email
-        self.salt = salt
-        self.key = key
-        self.time = time
-        self.admin = admin
-        self.validated = validated
-        self.password = password
+def check_session(username, id):
+    cursor.execute("SELECT session_id FROM users WHERE username = ?", (username,))
+    db_id = cursor.fetchone()[0]
+    print(base64.b64decode(id))
+    print(base64.b64decode(db_id))
+    if base64.b64decode(id) == base64.b64decode(db_id[0]):
+        return True
+    return False
     
-    def register(self):
-        self.__get_key()
-        try:
-            cursor.execute("INSERT INTO users VALUES (?,?,?,?,?,?,?)", (
-                self.username,
-                self.email,
-                self.salt,
-                self.key,
-                time(),
-                self.admin,
-                self.validated,)
-            )
+
+def __validate_input(username, user_email, password):
+    problems = []
+    temp_email = email.utils.parseaddr(user_email)
+    if temp_email == ('', ''):
+        problems.append("Invalid email address")
+    if len(password) > 100:
+        problems.append("Password must be less than 100 characters")
+    if len(password) < 8:
+        problems.append("Password must be greater than 8 characters")
+    if len(username) > 30:
+        problems.append("Username must be less than 30 characters")
+    if len(username) < 4:
+        problems.append("Username must be greater than 4 characters")
+    return problems
+
+def register(username, user_email, password):
+    problems = __validate_input(username, user_email, password)
+    if problems != []:
+        return problems
+    salt = os.urandom(16) # Salt should be about 16 or more bytes
+    key = __get_key(password, salt)
+    creationtime = time.time()
+    try:
+        cursor.execute("INSERT INTO users (username,email,salt,key,creationtime,admin,validated) VALUES (?,?,?,?,?,?,?)", (
+            username,
+            user_email,
+            salt,
+            key,
+            creationtime,
+            False,
+            False)
+        )
+        connection.commit()
+    except sqlite3.IntegrityError as error:
+        problems.append("Username already taken")
+    return problems
+        
+def login(username, password):
+    try:
+        cursor.execute("SELECT salt, key FROM users WHERE username = ?", (username,))
+        salt, db_key = cursor.fetchone() 
+        user_key = __get_key(password, salt)
+        if db_key == user_key:
+            logintime = time.time()
+            session_id = base64.b64encode(os.urandom(64))
+            cursor.execute("UPDATE users SET logintime = ?, session_id = ?  WHERE username = ?", (logintime,session_id,username))
             connection.commit()
-            return True
-        except sqlite3.IntegrityError:
-            # Duplicate username detected
-            return False
-            
-    def login(self):
-        try:
-            cursor.execute("SELECT salt, key FROM users WHERE username=?", (self.username,))
-            salt, key = cursor.fetchone()
-            
-            self.__get_key()
-            
-            if self.key == key:
-                print("LOG HIM IN!!")
-            else:
-                print("GET ON OUT!!")
+            return session_id
+        else:
+            return "Incorrect password"
+    except TypeError as error:
+        return "User not found"
 
-        except sqlite3.IntegrityError:
-            print("uh oh")
-
-    def __get_key(self):
-        # As of 2013, at least 100,000 iterations of SHA-256 are suggested
-        self.key = hashlib.pbkdf2_hmac('sha256', self.password, self.salt, 100000)
+def __get_key(password, salt):
+    # As of 2013, at least 100,000 iterations of SHA-256 are suggested
+    key = hashlib.pbkdf2_hmac('sha256', password, salt, 100000)
+    return key
--- website.py
+++ website.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 app = Flask(__name__, static_url_path='/static')
 
 
@@ -15,40 +15,50 @@ def index():
     time = time.strftime("%c") # Locale’s appropriate date and time representation
     return render_template('index.html', time=time)
 
+@app.route('/auth')
+def auth():
+    username = request.cookies.get('username')
+    id = request.cookies.get('id')
+    if sqlite_db.check_session(username, id) == True:
+        return "Authenticated"
+    else:
+        return "Denied access"
 
 @app.route('/login', methods=['POST'])
 def login():
     post_data = request.form
-    
     username = post_data['username']
-    password = str.encode(post_data['password']) # Convert to bytes
+    password = str.encode(post_data['password']) # Convert plaintext to bytes
     
-    user = sqlite_db.User(username=username, password=password)
-    login_attempt = user.login()
+    login_request = sqlite_db.login(username, password)
     
-    
-    yep = "hello"
-    return yep
+    if type(login_request) != str:
+        resp = make_response("Log in successful")
+        resp.set_cookie('username', username)
+        resp.set_cookie('id', login_request)
+        return resp
+        #return "Log in successful"
+    else:
+        return login_request
 
 
 @app.route('/register', methods=['POST'])
 def register():
     post_data = request.form
-    
-    salt = urandom(16)
     username = post_data['username']
-    password = post_data['password']
-    password = str.encode(password) # Convert to bytes
+    password = str.encode(post_data['password']) # Convert plaintext to bytes
     email = post_data['email']
- 
-    
-    user = sqlite_db.User(username, email=email, salt=salt, password=password)
-    user.register()
-    
-    
-    yep = "hello"
-    return yep
+    problems = sqlite_db.register(username, email, password)
+    if problems == []:
+        return "Registration successful"
+    else:
+        return str(problems)
 
-    
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    print("Closing SQLite connection")
+    sqlite_db.connection.close
