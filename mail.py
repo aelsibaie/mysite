3350d7426ca5f7457c3105957ff21dea50e9172e
Amir El-Sibaie <amir.elsibaie@gmail.com>
2016-10-19 13:10:34 -0400

    mail2 is a proper MIME formatted mail sender

--- mail.py
+++ mail.py
@@ -1,15 +1,19 @@
+# Import smtplib for the actual sending function
 import smtplib
 
+# Import the email modules we'll need
+from email.mime.text import MIMEText
 
-fromaddr = "pi@aelsi.ddns.us"
-toaddrs = "amir.elsibaie@gmail.com"
-msg = "test_123"
+msg = MIMEText('I need you to tell me if anaything is wrong with this email.')
+# me == the sender's email address
+# you == the recipient's email address
+msg['Subject'] = 'NICK this is a test 1 - amir'
+msg['From'] = 'amir@aelsi.net'
+msg['To'] = 'WHATEVER@NICKEU.COM'
 
-server = smtplib.SMTP('192.168.1.14', 587)
-server.set_debuglevel(1)
-server.starttls()
-
-#server.login('raspberry', '')
-
-server.sendmail(fromaddr, toaddrs, msg)
-
+# Send the message via our own SMTP server.
+s = smtplib.SMTP('mail.aelsi.net', 587)
+s.starttls()
+s.login('amir@aelsi.net', 'Rock24!7')
+s.send_message(msg)
+s.quit()
