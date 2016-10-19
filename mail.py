f6eb8ea43550c09dc78b135fd18cb3634b0369e0
Amir El-Sibaie <amir.elsibaie@gmail.com>
2016-10-19 16:03:32 -0400

    local test mail

--- mail.py
+++ mail.py
@@ -1,15 +1,23 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
-fromaddr = "pi@aelsi.ddns.us"
-toaddrs = "amir.elsibaie@gmail.com"
-msg = "test_123"
+fromaddr = "amir@aelsi.net"
+toaddr = "amir.elsibaie@gmail.com"
 
-server = smtplib.SMTP('192.168.1.14', 587)
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "validitity sdasdas2"
+body = "Test of validitity "
+msg.attach(MIMEText(body, 'plain'))
+
+server = smtplib.SMTP('mail.aelsi.net', 587)
 server.set_debuglevel(1)
 server.starttls()
+server.login(fromaddr, 'Rock24!7')
 
-#server.login('raspberry', '')
-
-server.sendmail(fromaddr, toaddrs, msg)
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
 
